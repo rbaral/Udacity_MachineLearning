411ebbb2a1c232d5f7822ee404c362b314fdedcb
Ramesh Baral <rameshrajbaral@yahoo.com>
2016-08-05 22:01:14 -0400

    svm related files added

--- edu/fiu/ml/svm/project/svm_author_id.py
+++ edu/fiu/ml/svm/project/svm_author_id.py
@@ -10,21 +10,42 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("../../tools/")
 from email_preprocess import preprocess
-
+from class_vis import prettyPicture, output_image
+import numpy as np
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+#slice the training data to see the time difference in training small data set
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C = 10000.0) # initialize a svm classifier
+t0 = time()
+clf.fit(features_train, labels_train) # train the classifier
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test) # make the prediction
+print "prediction time:", round(time()-t0, 3), "s"
+#find the predictions of some elements
+#print pred[10], pred[26], pred[50]
+#find the number of test elements, predicted for Chris (1) class
+chrisClass = np.where((np.array(pred))>0)[0]
+print "ChrisClass count:", len(chrisClass)
+# now find the accuracy of the prediction
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print "accuracy is:",accuracy
 #########################################################
-
+### draw the decision boundary with the text points overlaid
+#prettyPicture(clf, features_test, labels_test)
+#output_image("test.png", "png", open("test.png", "rb").read())
 
--- edu/fiu/ml/svm/tutorial/StudentMain.py
+++ edu/fiu/ml/svm/tutorial/StudentMain.py
@@ -1,1 +1,41 @@
 __author__ = 'rbaral'
+
+import sys
+from class_vis import prettyPicture, output_image
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+########################## SVM #################################
+### we handle the import statement and SVC creation for you here
+from sklearn.svm import SVC
+# the more the value of C, the more accuracy is obtained
+# the combination of the kernel, C and gamma parameter can make the model overfit to the data
+clf = SVC(kernel="rbf", C=10000.0, gamma= 100.0)
+
+
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+clf.fit(features_train, labels_train)
+
+#### store your predictions in a list named pred
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print "accuracy is:", acc
+
+### draw the decision boundary with the text points overlaid
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
+
+def submitAccuracy():
+    return acc
