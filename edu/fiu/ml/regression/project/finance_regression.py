0c98055dde2d35fb0b075f92c2d73b4d890e6167
Ramesh Baral <rameshrajbaral@yahoo.com>
2016-08-19 19:59:26 -0400

    regression related files added

--- edu/fiu/ml/regression/project/finance_regression.py
+++ edu/fiu/ml/regression/project/finance_regression.py
@@ -15,56 +15,104 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
+sys.path.append("../../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../../../../../data/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
-### list will be the "target" feature
-features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
-target, features = targetFeatureSplit( data )
+def regressionBonusAndSalary():
+    ### list the features you want to look at--first item in the
+    ### list will be the "target" feature
+    features_list = ["bonus", "salary"]
+    data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+    #, sort_keys = '../../tools/python2_lesson06_keys.pkl'
+    target, features = targetFeatureSplit( data )
+    #print target
+    #print features
 
-### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
-feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
-train_color = "b"
-test_color = "b"
+    ### training-testing split needed in regression, just like classification
+    from sklearn.cross_validation import train_test_split
+    feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+    train_color = "b"
+    test_color = "r"
+
+    ### Your regression goes here!
+    ### Please name it reg, so that the plotting code below picks it up and
+    ### plots it correctly. Don't forget to change the test_color above from "b" to
+    ### "r" to differentiate training points from test points.
+    from sklearn import linear_model
+    ### name your regression reg
+    reg = linear_model.LinearRegression()
+
+    ### your code goes here!
+    reg.fit(feature_train, target_train)
+    #find the slope
+    print reg.coef_
+    #find the intercept
+    #print reg.intercept_
+    #find score the performance on the train data
+    #print reg.score(feature_train, target_train)
+    # find score on the test data
+    print reg.score(feature_test, target_test)
+    ### draw the scatterplot, with color-coded training and testing points
+    import matplotlib.pyplot as plt
+    for feature, target in zip(feature_test, target_test):
+        plt.scatter( feature, target, color=test_color )
+    for feature, target in zip(feature_train, target_train):
+        plt.scatter( feature, target, color=train_color )
+
+    ### labels for the legend
+    plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+    plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+    ### draw the regression line, once it's coded
+    try:
+        plt.plot( feature_test, reg.predict(feature_test), color="r" )
+    except NameError:
+        pass
+    reg.fit(feature_test, target_test)
+    plt.plot(feature_train, reg.predict(feature_train), color="b")
+    #slope of the new regression line
+    print reg.coef_
+    plt.xlabel(features_list[1])
+    plt.ylabel(features_list[0])
+    plt.legend()
+    plt.show()
+
+'''
+regression of bonus against the long term incentive
+'''
+def regressionBonusAndLongTermInc():
+    ### list the features you want to look at--first item in the
+    ### list will be the "target" feature
+    features_list = ["bonus", "long_term_incentive"]
+    data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+    #, sort_keys = '../../tools/python2_lesson06_keys.pkl'
+    target, features = targetFeatureSplit( data )
+    #print target
+    #print features
+
+    ### training-testing split needed in regression, just like classification
+    from sklearn.cross_validation import train_test_split
+    feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+    train_color = "b"
+    test_color = "r"
+
+    ### Your regression goes here!
+    ### Please name it reg, so that the plotting code below picks it up and
+    ### plots it correctly. Don't forget to change the test_color above from "b" to
+    ### "r" to differentiate training points from test points.
+    from sklearn import linear_model
+    ### name your regression reg
+    reg = linear_model.LinearRegression()
+
+    ### your code goes here!
+    reg.fit(feature_train, target_train)
+    #find the score on the test data
+    print reg.score(feature_test, target_test)
 
 
-
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
+if __name__=="__main__":
+    #regressionBonusAndLongTermInc()
+    regressionBonusAndSalary()
 
 
-
-
-
-
-
-
-### draw the scatterplot, with color-coded training and testing points
-import matplotlib.pyplot as plt
-for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
-for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
-
-### labels for the legend
-plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
-plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
-
-
-
-
-### draw the regression line, once it's coded
-try:
-    plt.plot( feature_test, reg.predict(feature_test) )
-except NameError:
-    pass
-plt.xlabel(features_list[1])
-plt.ylabel(features_list[0])
-plt.legend()
-plt.show()
--- edu/fiu/ml/regression/tutorial/StudentMain.py
+++ edu/fiu/ml/regression/tutorial/StudentMain.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python
 __author__ = 'rbaral'
+
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+
+import matplotlib.pyplot as plt
+from studentRegression import studentReg
+from class_vis import prettyPicture, output_image
+
+from ages_net_worths import ageNetWorthData
+
+ages_train, ages_test, net_worths_train, net_worths_test = ageNetWorthData()
+
+reg = studentReg(ages_train, net_worths_train)
+
+#predict the networth for a sample age
+sampleData = np.array(29).reshape(-1,1) # this was required to avoid the deprecated warning message
+print "At age 29, the predicted networth is:",reg.predict(sampleData)
+#get the slope
+print "slope is:",reg.coef_
+#get the intercept
+print "intercept is:",reg.intercept_
+#get the r-squared for the train and test data
+r_squared_train = reg.score(ages_train, net_worths_train)
+print "r_squared_train is:",r_squared_train
+r_squared_test = reg.score(ages_test, net_worths_test)
+print "r_squared_test:",r_squared_test
+plt.clf()
+plt.scatter(ages_train, net_worths_train, color="b", label="train data")
+plt.scatter(ages_test, net_worths_test, color="r", label="test data")
+plt.plot(ages_test, reg.predict(ages_test), color="black")
+plt.legend(loc=2)
+plt.xlabel("ages")
+plt.ylabel("net worths")
+
+
+plt.savefig("test.png")
+output_image("test.png", "png", open("test.png", "rb").read())
