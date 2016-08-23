7f541e957a889ac231b5e3554a7208f799fe6073
Ramesh Baral <rameshrajbaral@yahoo.com>
2016-08-23 14:24:15 -0400

    email parsing for text learning project was added

--- edu/fiu/ml/tools/parse_out_email_text.py
+++ edu/fiu/ml/tools/parse_out_email_text.py
@@ -26,25 +26,40 @@ def parseOutText(f):
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        # get the email-sender using the From: keyword
+        emailSender = content[0][content[0].index('From:'):content[0].index('From:')+20]
 
         ### project part 2: comment out the line below
-        words = text_string
+        #print text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        words = text_string.split()
+        from nltk.stem import SnowballStemmer
+        stemmer = SnowballStemmer("english")
 
+        stemmedWords = []
+        for word in words:
+            stemmedWords.append(stemmer.stem(word))
+        #prepare the new text
+        stemmedText = ""
+        for word in stemmedWords:
+            if len(word.strip())>0: # avoid unnecessary white spaces
+                stemmedText+=word.strip()+" "
+        words = stemmedText
 
-
+        #stemmed = [stemmer.stem(w) for w in text_string.split()]
+        #words = " ".join(stemmed)
 
     return words
 
     
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
-    text = parseOutText(ff)
+    ff = open("../text_learning/project/test_email.txt", "r")
+    sender, text = parseOutText(ff)
+    #print sender
     print text
 
 
